fdf1e066b5a5aaab61d962973c83a446c2e01f2a
Alex Borst <butler.borst@gmail.com>
2019-04-18 21:31:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   name_hash = {
+     :x => 1,
+     :y => 2,
+     :z => 3
+   }
+   
+   name_hash.collect 
+   
 end
