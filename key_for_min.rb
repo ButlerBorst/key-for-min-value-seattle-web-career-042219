ebd4ef0dc848caabe3660c696ea6e4f2f6f65e5a
Alex Borst <butler.borst@gmail.com>
2019-04-18 21:38:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,14 @@ def key_for_min_value(name_hash)
      :z => 3
    }
    
-   name_hash.each do|key, value|
-    puts key if value == name_hash.values.key_for_min_value
+   ke
+   
+   if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
   end
+  lowest_key
 end
    
    
